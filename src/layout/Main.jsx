d75760705f6f710ac23ad097cce83dea401414ea
MostakemHossain <mostakememon123@gmail.com>
2023-11-26 23:35:16 +0600

    naver added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navber from '../components/Navber'
 
 const Main = () => {
   return (
     <div>
-        
+      <Navber/>  
         <Outlet/>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,10 @@ export default {
      
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ['light'],
+  },
 }
+
 
